b42d06e59f49c642280b85f7d9f63b33d6614354
SUGACAT <kaner820@gmail.com>
2023-04-21 16:25:25 +0900

    TileMap

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -19,15 +19,23 @@ public class PlayerMove : MonoBehaviour
     void Update()
     {
         //Jump
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("isJumping"))
         {
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
             anim.SetBool("isJumping", true);
         }
+        
+        //No Double Click
+        if (Input.GetKey(KeyCode.LeftArrow)&&Input.GetKey(KeyCode.RightArrow))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0, rigid.velocity.y);
+
+        }
+        
             
         
         //Stop Speed
-    if (Input.GetButtonUp("Horizontal"))
+        if (Input.GetButtonUp("Horizontal"))
             rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
 
         //Direction Sprite
@@ -51,14 +59,15 @@ public class PlayerMove : MonoBehaviour
         else if (rigid.velocity.x < maxSpeed*(-1))//Left Max Speed
             rigid.velocity = new Vector2(maxSpeed*(-1), rigid.velocity.y);
         //Landing Platform
-        Debug.DrawRay(rigid.position, Vector3.down, new Color(0,1,0));
-
-        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
-
-        if (rayHit.collider != null)
+        if (rigid.velocity.y < 0)
         {
-            if(rayHit.distance < 0.5f)
-                anim.SetBool("isJumping",false);
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0,1,0));
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+            if (rayHit.collider != null)
+            {
+                if(rayHit.distance < 0.5f)
+                    anim.SetBool("isJumping",false);
+            }
         }
     }
 }
