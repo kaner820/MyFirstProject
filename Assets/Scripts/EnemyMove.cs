44d79dae37c0c8a2a3f1f1acc60e2a4aaddebda4
SUGACAT <kaner820@gmail.com>
2023-04-25 11:49:53 +0900

    EnemyMove

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    private Rigidbody2D rigid;
+    private Animator anim;
+    private SpriteRenderer spriteRenderer;
+    public int nextMove;
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Invoke("Think", 5);
+    }
+    void FixedUpdate()
+    {
+        //Move
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+        
+        //Platform Check
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove*0.2f, rigid.position.y);
+        Debug.DrawRay(frontVec, Vector3.down, new Color(0,1,0));
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
+        if (rayHit.collider == null)
+        {
+            nextMove *= -1;
+            CancelInvoke();
+            Invoke("Think", 5);
+        }
+    }
+
+    //재귀함수
+    void Think()
+    {
+        //Set Next Active
+        nextMove = Random.Range(-1, 2);
+
+
+        
+        //Sprite Animation
+        anim.SetInteger("WalkSpeed", nextMove);
+        
+        //Flip Sprite
+        if (nextMove != 0)
+            spriteRenderer.flipX = nextMove == 1;
+        
+        //Recursive
+        float nextThinkTime = Random.Range(2f, 5f);
+        Invoke("Think", nextThinkTime);
+    }
+}
